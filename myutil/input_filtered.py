3ef26a1d6337124dee55a404f9db1aeeddd62c79
polygon1117 <hnjm4219@yahoo.co.jp>
2018-10-17 10:00:54 +0900

    add docstring

--- myutil/input_filtered.py
+++ myutil/input_filtered.py
@@ -1,10 +1,14 @@
+
+
 def int_input(min_val=None, max_val=None, description="",
               min_error_message="default",
               max_error_message="default",
               value_error_message="default"):
     """
     Read a string from standard input. And transform a string integer.
-    If integer is smaller than min_val or integer is bigger than max_val or a string is not integer,
+
+    If integer is smaller than min_val or integer is bigger than max_val
+    or a string is not integer,
     read a string from standard input again.
 
     Parameters
@@ -29,26 +33,42 @@ def int_input(min_val=None, max_val=None, description="",
     -------
     input_int : int
         Input value.
+
     """
+    def filter_inputs_by_min(inputs_int):
+        if min_val is not None and inputs_int < min_val:
+            if min_error_message == "default":
+                print("Input value should be bigger\
+                 than or equal {}.".format(min_val))
+            elif min_error_message:
+                print(min_error_message)
+            return None
+        return inputs_int
+
+    def filter_inputs_by_max(inputs_int):
+        if max_val is not None and inputs_int > max_val:
+            if max_error_message == "default":
+                print("Input value should be smaller\
+                 than or equal {}.".format(max_val))
+            elif max_error_message:
+                print(max_error_message)
+            return None
+        return inputs_int
+
+    def filter_inputs_by_min_max(inputs):
+        inputs_int = int(inputs)
+        inputs_filtered_min = filter_inputs_by_min(inputs_int)
+        inputs_filtered = filter_inputs_by_max(inputs_filtered_min)
+        return inputs_filtered
+
     while True:
         inputs = input(description)
         try:
-            inputs_int = int(inputs)
-            if min_val is not None and inputs_int < min_val:
-                if min_error_message == "default":
-                    print("Input value should be bigger than or equal {}.".format(min_val))
-                elif min_error_message:
-                    print(min_error_message)
+            inputs_int = filter_inputs_by_min_max(inputs)
+            if inputs_int is None:
                 continue
-
-            if max_val is not None and inputs_int > max_val:
-                if max_error_message == "default":
-                    print("Input value should be smaller than or equal {}.".format(max_val))
-                elif max_error_message:
-                    print(max_error_message)
-                continue
-
-            return inputs_int
+            else:
+                return inputs_int
         except ValueError:
             if value_error_message == "default":
                 print("Input value should be integer")
@@ -58,11 +78,34 @@ def int_input(min_val=None, max_val=None, description="",
 
 
 def true_or_false_input(true_val, false_val, description="", lower=True):
+    """
+    Read a string from standard input. And transform into bool val.
+
+    If input equals true_val or false_val, return True or False,
+    otherwise read a string againself.
+
+    Parameters
+    ----------
+    true_val : str
+        True word
+    false_val : str
+        False word
+    description : str
+        Use like input(description)
+    lower : bool
+        Transfomr input string to lower case
+
+    Returns
+    -------
+    true_or_false : bool
+        True or False
+
+    """
     while True:
         inputs = input(description)
         if lower:
             inputs = inputs.lower()
-            
+
         if inputs == true_val:
             return True
         if inputs == false_val:
@@ -71,7 +114,30 @@ def true_or_false_input(true_val, false_val, description="", lower=True):
             print('Input {} or {}'.format(true_val, false_val))
 
 
-def alphabets_input(min_length=None, max_length=None, description="", filter_null=True):
+def alphabets_input(min_length=None, max_length=None,
+                    description="", filter_null=True):
+    """
+    Read a string from standard. And filter input by length.
+
+    If length of input is out of defined length, read a string again.
+
+    Parameters
+    ----------
+    min_length : int
+        Minimum length of input. None means no minimum length.
+    max_length : int
+        Maximum length of input. None means no maximum length.
+    description : str
+        Use like input(description)
+    filter_null : bool
+        If return input "". If False and input equals "", read again.
+
+    Returns
+    -------
+    alphabets_input : str
+        Only alphabet string or "" (only if filter_null is False).
+
+    """
     while True:
         inputs = input(description)
         if not filter_null and len(inputs) == 0:
--- myutil/text_effect.py
+++ myutil/text_effect.py
@@ -3,6 +3,23 @@ import sys
 
 
 def type_print(s, interval=0.1, end="\n"):
+    """
+    Print string like type.
+
+    Parameters
+    ----------
+    s : str
+        Print string
+    interval : float
+        Type interval (second)
+    end : str
+        Like print(s, end)
+
+    Returns
+    -------
+    None
+    
+    """
     for c in s:
         print(c, end="")
         sys.stdout.flush()
